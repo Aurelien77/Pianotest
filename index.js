e7fa381e53da9bd953832784682846806015765f
Aurélien <aurelien.monceau@gmail.com>
2021-11-19 23:10:23 +0100

    fixed bugs

--- index.js
+++ index.js
@@ -9,18 +9,6 @@ const db = require("./models");
 
 global.__basedir = __dirname;
 
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, X-Requested-With, Authorization"
-  );
-  if (req.method === "OPTIONS") res.send(200);
-  else next();
-};
-
-app.use(allowCrossDomain);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
