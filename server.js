bb627373d4170950e850bbfa24362815d3c12901
Aurélien <aurelien.monceau@gmail.com>
2021-11-19 21:01:54 +0100

    fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log("Server running on port 3001");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
 
 server.listen(port);
