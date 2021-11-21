8e169fc67977643055483b6f0a06a8dfe3c8930f
Aurélien <aurelien.monceau@gmail.com>
2021-11-21 14:12:37 +0100

    bug

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const cors = require("cors");
 const helmet = require("helmet");
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "Access-Control-Allow-Origin: *",
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 const db = require("./models");
 
