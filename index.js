2083fc4bf5d2c4c2dee3b6f9db70a5f5e6d8d46e
Aurélien <aurelien.monceau@gmail.com>
2021-11-21 14:23:24 +0100

    bug

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
