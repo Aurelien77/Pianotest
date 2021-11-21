93ee4b4b08fbe6ca31915890e9a4586095ed0dd1
Aurélien <aurelien.monceau@gmail.com>
2021-11-21 15:47:19 +0100

    test

--- index.js
+++ index.js
@@ -4,30 +4,24 @@ const cors = require("cors");
 const helmet = require("helmet");
 app.use(express.json());
 
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 const db = require("./models");
 
 global.__basedir = __dirname;
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-
-  app.options("*", (req, res) => {
-    // allowed XHR methods
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
-    );
-    res.send();
-  });
-});
 
 // Routers
 const postRouter = require("./routes/Posts");
